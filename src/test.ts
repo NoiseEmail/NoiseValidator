9c4fb58a4720b138614f74d6638e2716ee2ce561
Grzegorz Maniak <git@grzegorz.ie>
2024-03-19 23:32:57 +0000

    Refactor code by commenting out unused imports, middleware, schemas, and routes

--- src/test.ts
+++ src/test.ts
@@ -1,81 +1,81 @@
-import {
-    Binder,
-    GenericMiddleware,
-    Schema,
-    Uuid,
-    Number,
-    String,
-    Optional,
-    Router,
-    Route
-} from 'gs';
+// import {
+//     Binder,
+//     GenericMiddleware,
+//     Schema,
+//     Uuid,
+//     Number,
+//     String,
+//     Optional,
+//     Router,
+//     Route
+// } from 'gs';
 
 
 
-class Test1Middleware extends GenericMiddleware<{
-    a: string
-}> {
+// class Test1Middleware extends GenericMiddleware<{
+//     a: string
+// }> {
     
-};
+// };
 
-class Test2Middleware extends GenericMiddleware<{
-    b: string
-}> {
+// class Test2Middleware extends GenericMiddleware<{
+//     b: string
+// }> {
     
-};
+// };
 
 
 
-const body_1_schema = new Schema.Body({
-    a: String,
-    b: Number,
-    c: Optional(Uuid)
-});
+// const body_1_schema = new Schema.Body({
+//     a: String,
+//     b: Number,
+//     c: Optional(Uuid)
+// });
 
-const body_2_schema = new Schema.Body({
-    a: String,
-    b: Number,
-    c: Uuid
-});
-
-
-
-const query_1_schema = new Schema.Query({
-    a: String,
-    b: Number,
-    c: Optional(Uuid)
-});
-
-const query_2_schema = new Schema.Query({
-    a: String,
-    b: Number,
-    c: Uuid
-});
+// const body_2_schema = new Schema.Body({
+//     a: String,
+//     b: Number,
+//     c: Uuid
+// });
 
 
-Router.instance.start({ debug: true });
-const basic_route = new Route('/basic', { friendly_name: 'Basic Route' });
+
+// const query_1_schema = new Schema.Query({
+//     a: String,
+//     b: Number,
+//     c: Optional(Uuid)
+// });
+
+// const query_2_schema = new Schema.Query({
+//     a: String,
+//     b: Number,
+//     c: Uuid
+// });
 
 
-Binder(basic_route, 'GET', {
-    middleware: {
-        test: Test1Middleware,
-        test2: Test2Middleware
-    },
-    schemas: {
-        body: body_1_schema,
-        query: query_1_schema,
-        output: body_1_schema,
-    }
-}, (data) => {
-    data.middleware.test.a;
+// Router.instance.start({ debug: true });
+// const basic_route = new Route('/basic', { friendly_name: 'Basic Route' });
 
-    data.body.a;
-    // data.url.id;
 
-    return {
-        a: 'a',
-        b: 1,
-        c: 'c'
-    }
-});
+// Binder(basic_route, 'GET', {
+//     middleware: {
+//         test: Test1Middleware,
+//         test2: Test2Middleware
+//     },
+//     schemas: {
+//         body: body_1_schema,
+//         query: query_1_schema,
+//         output: body_1_schema,
+//     }
+// }, (data) => {
+//     data.middleware.test.a;
+
+//     data.body.a;
+//     // data.url.id;
+
+//     return {
+//         a: 'a',
+//         b: 1,
+//         c: 'c'
+//     }
+// });
