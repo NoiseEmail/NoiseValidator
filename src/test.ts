c6fa12a3fdc469116473db9f0a9863dd2537e8ca
Grzegorz Maniak <git@grzegorz.ie>
2024-03-23 15:31:28 +0000

    Commented out testing file

--- src/test.ts
+++ src/test.ts
@@ -1,68 +1,68 @@
-import {
-    Binder,
-    GenericMiddleware,
-    Schema,
-    Uuid,
-    Number,
-    String,
-    Optional,
-    Router,
-    Route
-} from 'gs';
-import { ExcludeNonOptional, ExtractOtional, Split } from './binder/types';
+// import {
+//     Binder,
+//     GenericMiddleware,
+//     Schema,
+//     Uuid,
+//     Number,
+//     String,
+//     Optional,
+//     Router,
+//     Route
+// } from 'gs';
+// import { ExcludeNonOptional, ExtractOtional, Split } from './binder/types';
 
 
 
-class Test1Middleware extends GenericMiddleware<{
-    a: string
-}> {
+// class Test1Middleware extends GenericMiddleware<{
+//     a: string
+// }> {
     
-};
+// };
 
-class Test2Middleware extends GenericMiddleware<{
-    b: string
-}> {
+// class Test2Middleware extends GenericMiddleware<{
+//     b: string
+// }> {
     
-};
+// };
 
 
 
-const body_1_schema = new Schema.Body({
-    a: String,
-    b: Number,
-    c: Optional(Number)
-});
+// const body_1_schema = new Schema.Body({
+//     a: String,
+//     b: Number,
+//     c: Optional(Number)
+// });
 
 
 
-Router.instance.start({ debug: true });
-const basic_route = new Route('/basic', { friendly_name: 'Basic Route' });
+// Router.instance.start({ debug: true });
+// const basic_route = new Route('/basic', { friendly_name: 'Basic Route' });
 
 
-Binder(basic_route, 'GET', {
-    schemas: {
-        output: body_1_schema,
-    }
-}, (data) => {
+// Binder(basic_route, 'GET', {
+//     schemas: {
+//         output: body_1_schema,
+//     }
+// }, (data) => {
 
 
-    return {
-        a: 'test',
-        b: 1,
-        c: 2
-    }
-});
+//     return {
+//         a: 'test',
+//         b: 1,
+//         c: 2
+//     }
+// });
 
 
-type test = Split<ExcludeNonOptional<{
-    a: string,
-    b: undefined | number,
-}>>;
+// type test = Split<ExcludeNonOptional<{
+//     a: string,
+//     b: undefined | number,
+// }>>;
 
 
-let b: test['optional'] = {
+// let b: test['optional'] = {
   
-};
-let c: test['required'] = {
+// };
+// let c: test['required'] = {
     
-};
+// };
