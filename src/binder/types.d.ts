605269a58893c7f804b9149ad5e72e1f96555138
Grzegorz Maniak <git@grzegorz.ie>
2024-05-25 20:10:09 +0100

    Created ObjectModifier namespace

--- src/binder/types.d.ts
+++ src/binder/types.d.ts
@@ -70,10 +70,10 @@ export type BinderCallbackObject<
     DynamicURLString extends string
 > = {
     middleware: Middleware.ParsedMiddlewareObject<Middleware>,
-    body: DeepMergeReturnTypes<CreateArray<Body>>,
-    query: DeepMergeReturnTypes<CreateArray<Query>>,
-    headers: DeepMergeReturnTypes<CreateArray<Headers>>,
-    cookies: DeepMergeReturnTypes<CreateArray<Cookies>>,
+    body: ObjectModifier.DeepMergeReturnTypes<ObjectModifier.CreateArray<Body>>,
+    query: ObjectModifier.DeepMergeReturnTypes<ObjectModifier.CreateArray<Query>>,
+    headers: ObjectModifier.DeepMergeReturnTypes<ObjectModifier.CreateArray<Headers>>,
+    cookies: ObjectModifier.DeepMergeReturnTypes<ObjectModifier.CreateArray<Cookies>>,
     url: DynamicURL.Extracted<DynamicURLString>,
 
     cookie_objects: Map<string, Cookie.Shape>,
@@ -115,14 +115,14 @@ export type BinderConfigurationSchema<
 
 
 type ExtractOutputSchemaHelperSplit<DeepMergeReturnTypes extends object
-> = SplitObject<GetOutputType<DeepMergeReturnTypes, DeepMergeReturnTypes>>
+> = ObjectModifier.SplitObject<ObjectModifier.GetOutputType<DeepMergeReturnTypes, DeepMergeReturnTypes>>
 
 type ExtractOutputSchemaHelperMerge<SplitObject extends { required: object, optional: object }
 > = SplitObject['required'] & SplitObject['optional'];
 
 export type ExtractOutputSchema<
     RawSchema extends Schema.SchemaLike<Schema.SchemaType> | Array<Schema.SchemaLike<Schema.SchemaType>>,
-> = ExtractOutputSchemaHelperMerge<ExtractOutputSchemaHelperSplit<DeepMergeReturnTypes<CreateArray<RawSchema>>>>
+> = ExtractOutputSchemaHelperMerge<ExtractOutputSchemaHelperSplit<ObjectModifier.DeepMergeReturnTypes<ObjectModifier.CreateArray<RawSchema>>>>
 
 
 
@@ -166,152 +166,219 @@ export type OptionalBinderConfiguration<
 
 
 
-
-export type IsArray<T> = T extends Array<unknown> ? true : false;
-export type CreateArray<T> = T extends Array<unknown> ? T : [T];
-
-
-type DeepMerge<T> = T extends object
-    ? {
-        [K in keyof T]: T[K] extends infer U
-            ? U extends object
-                ? DeepMerge<U>
-                    : U
-                        : never;
-        }
-    : never;
-
-type DeepMergeReturnTypes<
-    Schemas extends Array<Schema.SchemaLike<Schema.SchemaType>>
-> = DeepMerge<
-    {
-        [K in keyof Schemas[number]]: Schemas[number][K] extends infer U
-        ? U extends Schema.SchemaLike<Schema.SchemaType>
-            ? U extends { _return_type: infer R }
-                ? R
-                    : never
-                        : DeepMerge<U>
-        : never;
-    }['_return_type']
->;
-
-
-
-
 export type BinderMap = Map<HTTPMethods, Array<BinderMapObject>>;
-type IsOptional<Input> = Input extends void | undefined ? true : false;
 
 
 
-/**
- * @name GetOutputType
- * @description If the input extends { [x: string]: never; }
- * than the output is void, otherwise the output is the input
- */
-export type GetOutputType<
-    Input extends Object,
-    ParsedOutput extends Object
-> =
-    Input extends { [x: string]: never; }
-        ? void
-        : ParsedOutput;
+export namespace ObjectModifier {
+
+
+    /**
+     * If the input is an array, return true, otherwise return false
+     */
+    export type IsArray<T> = T extends Array<unknown> ? true : false;
 
 
 
-// -- Man, I really fucking hated making these.
-export type DeepExcludeNonOptional<T> = {
-    [K in keyof T]: 
-        T[K] extends object ? DeepExcludeNonOptional<T[K]> : 
-            Extract<T[K], undefined> extends never ? never : T[K]
+    /**
+     * If the input is an array, return the input, otherwise return an array
+     * with the input as the only element.
+     */
+    export type CreateArray<T> = T extends Array<unknown> ? T : [T];
+
+
+    
+    /**
+     * Recursively merges deeply nested properties of an object type `T`.
+     * If a property is an object, it will be merged recursively.
+     * Otherwise, the property type is preserved.
+     * If `T` is not an object, it results in `never`.
+     */
+    export type DeepMerge<T> = T extends object
+        ? {
+            [K in keyof T]: T[K] extends infer U
+                ? U extends object
+                    ? DeepMerge<U>
+                        : U
+                            : never;
+            }
+        : never;
+
+
+
+    /**
+     * Given an array of schemas, it will return the return type of the
+     * schemas.
+     * 
+     * IN: { a: { _return_type: string }, b: { _return_type: number } }
+     * OUT: { a: string, b: number }
+     */
+    export type DeepMergeReturnTypes<
+        Schemas extends Array<Schema.SchemaLike<Schema.SchemaType>>
+    > = DeepMerge<
+        {
+            [K in keyof Schemas[number]]: Schemas[number][K] extends infer U
+            ? U extends Schema.SchemaLike<Schema.SchemaType>
+                ? U extends { _return_type: infer R }
+                    ? R
+                        : never
+                            : DeepMerge<U>
+            : never;
+        }['_return_type']
+    >;
+
+
+
+    /**
+     * Check if the input is optional, if it is, return true, otherwise return false
+     */
+    export type IsOptional<Input> = Input extends void | undefined ? true : false;
+
+
+
+    /**
+     * If the input extends { [x: string]: never; }
+     * than the output is void, otherwise the output is the input
+     */
+    export type GetOutputType<
+        Input extends Object,
+        ParsedOutput extends Object
+    > = Input extends { [x: string]: never; } ? void : ParsedOutput;
+
+
+
+    //
+    // -- Man, I really fucking hated making these.
+    //
+
+
+
+    /**
+     * DeepExcludeNonOptional recursively excludes non-optional properties from an object type `T`.
+     * If a property is an object, it will be processed recursively.
+     * If a property is not optional (does not include undefined), it will be excluded (results in never).
+     */
+    export type DeepExcludeNonOptional<T> = {
+        [K in keyof T]: 
+            T[K] extends object ? DeepExcludeNonOptional<T[K]> : 
+                Extract<T[K], undefined> extends never ? never : T[K]
+    };
+
+
+
+    /**
+     * DeepExcludeOptional recursively excludes optional properties from an object type `T`.
+     * If a property is an object, it will be processed recursively.
+     * If a property is optional (includes undefined), it will be excluded (results in never).
+     */
+    export type DeepExcludeOptional<T> = {
+        [K in keyof T]: 
+            T[K] extends object ? DeepExcludeOptional<T[K]> : 
+                Extract<T[K], undefined> extends never ? T[K] : never
+    };
+
+
+
+    /**
+     * DeepOmit recursively omits properties of a certain type from an object type `T`.
+     * If a property is an object, it will be processed recursively.
+     * If a property is of type `OmitType`, it will be omitted (results in never).
+     */
+    export type DeepOmit<T, OmitType> = {
+        [K in keyof T as T[K] extends OmitType ? never : K]: 
+            T[K] extends object ? DeepOmit<T[K], OmitType> : T[K];
+    };
+    
+
+
+    /**
+     * DeepPartial makes all properties in `T` optional and allows undefined.
+     * If a property is an object, it will be processed recursively.
+     */
+    export type DeepPartial<T> = {
+        [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K] | undefined;
+    };
+
+
+
+    /**
+     * Given an object, it will return a new object with all the keys
+     * that have a value of `undefined` Marked as optional.
+     * 
+     * @example
+     * type Input = {
+     *   a: string,
+     *   b: undefined | number,
+     * }
+     * 
+     * type Output = OmitNonOptional<Input>;
+     * Output = {
+     *    b: number | undefined
+     * }
+     */
+    export type DeepOptional<Input> = DeepPartial<DeepOmit<DeepExcludeNonOptional<Input>, never>>;
+
+
+
+    /**
+     * Given an object, it will return a new object with all the keys
+     * that are optional removed.
+     * 
+     * @example
+     * type Input = {
+     *   a: string,
+     *   b?: number,
+     * }
+     * 
+     * type Output = OmitOptional<Input>;
+     * Output = {
+     *   a: string
+     * }
+     */
+    export type DeepRequired<Input> = DeepOmit<DeepExcludeOptional<Input>, undefined>;
+
+
+    
+    /**
+     * Given an object, it will return an object with two keys:
+     * `required` and `optional`. The `required` key will contain all the keys
+     * that are required, and the `optional` key will contain all the keys that
+     * are optional.
+     * 
+     * @example
+     * type Input = {
+     *  a: string,
+     *  b?: number | undefined,
+     * }
+     * 
+     * type Output = SplitObject<Input>;
+     * Output = {
+     *    required: { a: string },
+     *    optional: { b: number | undefined }
+     * }
+     */
+    export type SplitObject<Input> = {
+        required: DeepRequired<Input>,
+        optional: DeepOptional<Input>
+    };
 }
-
-export type DeepExcludeOptional<T> = {
-    [K in keyof T]: 
-        T[K] extends object ? DeepExcludeOptional<T[K]> : 
-            Extract<T[K], undefined> extends never ? T[K] : never
-}
-
-export type DeepOmit<T, OmitType> = {
-    [K in keyof T as T[K] extends OmitType ? never : K]: 
-        T[K] extends object ? DeepOmit<T[K], OmitType> : T[K];
-};
-  
-export type DeepPartial<T> = {
-    [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K] | undefined;
-};
-
-
-
-/**
- * @name DeepOptional
- * @description Given an object, it will return a new object with all the keys
- * that have a value of `undefined` Marked as optional.
- * 
- * @example
- * type Input = {
- *   a: string,
- *   b: undefined | number,
- * }
- * 
- * type Output = OmitNonOptional<Input>;
- * Output = {
- *    b: number | undefined
- * }
- */
-export type DeepOptional<Input> = DeepPartial<DeepOmit<DeepExcludeNonOptional<Input>, never>>;
-
-
-
-/**
- * @name DeepRequired
- * @description Given an object, it will return a new object with all the keys
- * that are optional removed.
- * 
- * @example
- * type Input = {
- *   a: string,
- *   b?: number,
- * }
- * 
- * type Output = OmitOptional<Input>;
- * Output = {
- *   a: string
- * }
- */
-export type DeepRequired<Input> = DeepOmit<DeepExcludeOptional<Input>, undefined>;
-
-
-  
-/**
- * @name SplitObject
- * @description Given an object, it will return an object with two keys:
- * `required` and `optional`. The `required` key will contain all the keys
- * that are required, and the `optional` key will contain all the keys that
- * are optional.
- * 
- * @example
- * type Input = {
- *  a: string,
- *  b?: number | undefined,
- * }
- * 
- * type Output = SplitObject<Input>;
- * Output = {
- *    required: { a: string },
- *    optional: { b: number | undefined }
- * }
- */
-export type SplitObject<Input> = {
-    required: DeepRequired<Input>,
-    optional: DeepOptional<Input>
-};
 
 
 
 export namespace Cookie {
+
+    /**
+     * Web docs:
+     * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie
+     */
     export type SameSite = 'strict' | 'lax' | 'none';
 
 
+
+    /**
+     * Web docs:
+     * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie
+     */
     export type Options = {
         domain: string,
         expires: Date,
@@ -324,6 +391,10 @@ export namespace Cookie {
     }
 
 
+
+    /**
+     * This is the shape of a cookie when setting it server-side.
+     */
     export type Shape = {
         value: string,
         options: Partial<Options>
