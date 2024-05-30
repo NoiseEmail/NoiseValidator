871598b8866d3ac68c539cdaae7f1984f9373bbd
Grzegorz Maniak <git@grzegorz.ie>
2024-05-30 21:09:52 +0100

    Added private registry

---
+++ package.sh
@@ -0,0 +1,11 @@
+# -- TSC Compile (Ignore Errors)
+tsc --noEmit --skipLibCheck
+echo "TSC Compile Done"
+
+# -- Copy over the package.json, README.md, and LICENSE
+cp package.json dist/package.json
+cp README.md dist/README.md
+cp LICENSE dist/LICENSE
+
+# -- Publish "registry": "http://localhost:4873"
+cd dist && npm publish --registry http://localhost:4873
